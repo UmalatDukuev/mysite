daba5f1bcb567501eea47385a72dbdcd4554284f
Umalat <dukuev037@mail.ru>
2024-08-25 22:42:39 +0300

    1

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 
 # Create your views here.
@@ -22,4 +25,10 @@ def signup(request):
         'form': form,
     })
 
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page.html'
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
-    path('accounts/', include('django.contrib.auth.urls'), name="auth"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('secret/', views.secret_page, name="secret"),
+    path('secret2/', views.SecretPage.as_view(), name="secret2"),
 
 ]
